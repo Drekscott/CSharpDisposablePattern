c881469480acf33fc5810243afc383d9987f9134
drekscott <drekscott@icloud.com>
2020-06-24 08:45:56 -0400

    Project to go through disposables and streams

---
+++ DisposablePattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace DisposablePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Create stream reader object
+            StreamReader streamReaderObject = default(StreamReader);
+
+            try
+            {
+                // Assigns "streamReaderObject" to read from a text file named "file"
+                streamReaderObject = new StreamReader("file.txt");
+
+                // Reads all characters from the current position to the end of the stream.
+                string fileContents = streamReaderObject.ReadToEnd();
+
+                // Close StreamReader
+                streamReaderObject.Close();
+
+                Console.Write(fileContents);
+
+                // Write amount of string elements to Console.
+                Console.WriteLine($"\nThe file has {new StringInfo(fileContents).LengthInTextElements}");
+
+
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("File can not be found");
+            }
+            // Invoking the Dispose method in a finally block
+            // Finally block will always execute
+            finally
+            {
+                if(streamReaderObject != null)
+                {
+                    // Release all resources. Call the Dispose method
+                    streamReaderObject.Dispose();
+                }
+            }
+
+        }
+    }
+}
